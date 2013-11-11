14206579114197fec215d5bfbe9e978c287efecf
Brent Raines <bt.raines@gmail.com>
2013-11-11 11:32:29 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Picklr::Application.config.secret_key_base = '573cf35a8e3478e7fac4a9a8066efa214bb899e4d53d3a0b72bd3e34ef6a8eb6ecf6f5370e665ffe84dd48dc809de16b605e2de6169cc88f1bdb22bbb95e6e15'
---
+++ spec/models/pickl_spec.rb
@@ -0,0 +1,14 @@
+require 'spec_helper'
+
+describe Pickl do
+
+  subject { Pickl.new }
+  
+  describe "body validation" do
+
+    it "only allows 150 characters" do
+      
+    end
+  end
+
+end
